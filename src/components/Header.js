f9c7b37e5a755e90839891ae1aed2eacafdd7cf5
ash-lol <ashyketchum1234@gmail.com>
2025-02-25 03:53:13 +0530

    Image addition and theme change

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,9 @@ import './Header.css';
 const Header = () => (
   <header>
     <h1>
-      Funny <span className="highlight">Dynamic</span> Web App
+      Pranay
+      <img src={`${process.env.PUBLIC_URL}/storage/bihrtday.jpg`} alt="Birthday" className="inline-image" />
+      Signature
     </h1>
     <p>Where humor meets tech!</p>
   </header>
